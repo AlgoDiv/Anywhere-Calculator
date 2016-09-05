bc339d9efd7b075ac101156c2061a0072c549d02
Fri00 <xfriomanx@gmail.com>
2016-09-04 23:23:18 -0500

    Update popup.js and popup.html

--- popup.js
+++ popup.js
@@ -3,6 +3,6 @@ function appEquat(x) {
   equation = equation + x;
 }
 function showEquat() {
-  document.getElementById("answer").innerHTML = eval(equation)
+  document.getElementById("answer").innerHTML = eval(equation);
 }
 document.getElementById("r1b3").onclick = showEquat();
