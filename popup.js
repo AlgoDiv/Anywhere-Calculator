c335a192eb23cb5a571b83efe7812ce7f825ed60
Tyler Johnson <xfriomanx@gmail.com>
2016-09-04 23:10:17 -0500

    Update and rename js/popup.js to popup.js

--- popup.js
+++ popup.js
@@ -1,7 +1,8 @@
-var equation = "";
+var equation = "0";
 function appEquat(x) {
   equation = equation + x;
 }
 function showEquat() {
   document.getElementById("answer").innerHTML = eval(equation)
 }
+document.getElementById("r1b3").onclick = showEquat();
