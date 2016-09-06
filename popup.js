9fd8062b66d8339b9f861b53523850f4720bf9ee
Tyler Johnson <xfriomanx@gmail.com>
2016-09-05 19:01:21 -0500

    Update popup.js

--- popup.js
+++ popup.js
@@ -5,4 +5,6 @@ function appEquat(x) {
 function showEquat() {
   document.getElementById("answer").innerHTML = eval(equation);
 }
+
+document.getElementById("r2b1").onclick = function() { appEquat("1"); };
 document.getElementById("r1b3").onclick = showEquat;
