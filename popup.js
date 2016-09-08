2005b2fb3fcfb0bb94fcf0a6e9e75f2692a584f3
Fri00 <xfriomanx@gmail.com>
2016-09-08 18:44:15 -0500

    Revert "Update popup.js"
    This reverts commit 787f723e8aa2a53a2449b70b8bd21a1365ef1f77.

--- popup.js
+++ popup.js
@@ -28,38 +28,13 @@ function getAns() {
       tempNum = answer;
       curNum = 0;
       updateAns();
-    case 'M':
-      answer = tempNum * curNum;
-      tempNum = answer;
-      curNum = 0;
-      updateAns();
-    case 'D':
-      answer = tempNum / curNum;
-      tempNum = answer;
-      curNum = 0;
-      updateAns();
   }
 }
 function updateAns() {
   document.getElementById("answer").innerHTML = answer;
 }
 
-document.getElementById("r1b1").onclick = function() { appCN("0"); };
-
 document.getElementById("r1b3").onclick = function() { getAns(); };
-document.getElementById("r1b4").onclick = function() { getAction('P'); };
-
 document.getElementById("r2b1").onclick = function() { appCN("1"); };
 document.getElementById("r2b2").onclick = function() { appCN("2"); };
-document.getElementById("r2b3").onclick = function() { appCN("3"); };
 document.getElementById("r2b4").onclick = function() { getAction('S'); };
-
-document.getElementById("r3b1").onclick = function() { appCN("4"); };
-document.getElementById("r3b2").onclick = function() { appCN("5"); };
-document.getElementById("r3b3").onclick = function() { appCN("6"); };
-document.getElementById("r3b4").onclick = function() { getAction('M'); };
-
-document.getElementById("r4b1").onclick = function() { appCN("7"); };
-document.getElementById("r4b2").onclick = function() { appCN("8"); };
-document.getElementById("r4b3").onclick = function() { appCN("9"); };
-document.getElementById("r4b4").onclick = function() { getAction('D'); };
