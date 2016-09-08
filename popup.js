f4311a6de348681ceca99680a461a11c2ff3d234
Tyler Johnson <xfriomanx@gmail.com>
2016-09-08 18:30:31 -0500

    Update popup.js

--- popup.js
+++ popup.js
@@ -7,9 +7,19 @@ function appCN(x) {
   answer = curNum;
   updateAns();
 }
+function getAction(x) {
+  action = x;
+  if (curNum != 0) {
+    tempNum = curNum
+  }
+  curNum = 0;
+  answer = curNum;
+  updateAns();
+}
 function updateAns() {
   document.getElementById("answer").innerHTML = answer;
 }
 
 document.getElementById("r2b1").onclick = function() { appCN("1"); };
 document.getElementById("r2b2").onclick = function() { appCN("2"); };
+document.getElementById("r2b4").onclick = function() { getAction('S'); };
