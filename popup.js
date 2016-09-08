caf062f19882a709e590544eea6f2a09709ff55c
Tyler Johnson <xfriomanx@gmail.com>
2016-09-08 18:26:31 -0500

    Update popup.js

--- popup.js
+++ popup.js
@@ -2,7 +2,7 @@ var curNum = 0;
 var tempNum = 0;
 var answer = 0;
 var action = 'A';
-function appCurNum(x) {
+function appCN(x) {
   curNum = parseInt(curNum.toString() + x);
   answer = curNum;
   updateAns();
@@ -11,5 +11,5 @@ function updateAns() {
   document.getElementById("answer").innerHTML = answer;
 }
 
-document.getElementById("r2b1").onclick = function() { appX("1"); };
-document.getElementById("r2b2").onclick = function() { appX("2"); };
+document.getElementById("r2b1").onclick = function() { appCN("1"); };
+document.getElementById("r2b2").onclick = function() { appCN("2"); };
